5f00269c43337706015cc299ecd8bc933a5dd0ac
Sathishkumar <skumarscse@gmail.com>
2022-06-30 21:38:39 +0530

    JavaScript intro

---
+++ 3-JavaScript/1-Intro/script.js
@@ -0,0 +1,7 @@
+document.write("____________________________________");
+document.write("<br />");
+document.write("I am from External JavaScript File");
+let a = 10;
+let b = 20;
+let c = a + b; // c = 10 + 20 => 30
+document.write(`The result is: ${c}`);
